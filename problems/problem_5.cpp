f9b8abc0b628d240857773f97bf6ef089b2e0e6b
sanjar0215 <131523769+sanjar0215@users.noreply.github.com>
2023-11-14 18:19:07 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,26 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
+    switch(operation) {
+        case '+':
+            result = x + y;
+            break;
+        case '-':
+            result = x - y;
+            break;
+        case '*':
+            result = x * y;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = x / y;
+            } else {
+                return 1;
+            }
+            break;
+        default:
+            return 1;
+    }
 
    return result;
 }
