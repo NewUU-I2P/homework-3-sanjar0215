3d044e5690aa2df89c1dae58a827f27ef27f93aa
sanjar0215 <131523769+sanjar0215@users.noreply.github.com>
2023-11-14 14:31:12 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,26 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    std::string macAddress;
+    std::getline(std::cin, macAddress);
 
+    std::stringstream ss(macAddress);
+    std::string hexNumber;
+    int firstOctet;
+
+    std::getline(ss, hexNumber, ':');
+    std::stringstream(hexNumber) >> std::hex >> firstOctet;
+
+
+    std::string addressType;
+    if (firstOctet % 2 == 0) {
+        addressType = "Unicast";
+    } else if (firstOctet == 0xFF) {
+        addressType = "Broadcast";
+    } else {
+        addressType = "Multicast";
+    }
+    return 0;
     // make use of control flow statements
     // return result;
 }
