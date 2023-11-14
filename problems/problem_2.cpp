65139c38d12192469a35ca07b3c307dbff4baeaa
sanjar0215 <131523769+sanjar0215@users.noreply.github.com>
2023-11-14 14:20:55 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,8 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
-
+    cout << "Enter three costs";
+    cin >> i >> j >> k;
+    result = (i > j) ? ( i > k ? i : k ) : ( j > k ? j : k );
     return result;
 }
