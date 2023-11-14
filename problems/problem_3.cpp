aa3a0bc057dea5c13393db4617cf4bf920fea71a
sanjar0215 <131523769+sanjar0215@users.noreply.github.com>
2023-11-14 17:54:02 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -4,17 +4,16 @@ std::string problemSolution3(float height, char S) {
     // write your code here
 
     if('M' == S && height < 1.70){
-        cout << "Short";
+        
     } else if(S == 'M' && (height >= 1.70 && height < 1.85)){
-        cout << "Normal";
+        
     } else if(S == 'M' && height >= 1.85){
-        cout << "Tall";
+        
     } else if(S=='F' && height < 1.60){
-        cout << "Short";
+        
     } else if(S == 'F' && (height >= 1.60 && height < 1.75)){
-        cout << "Normal";
+        
     } else if(S == 'F' && height >= 1.75){
-        cout << "Tall";
     }
     return 0;
     // use return to return your result
