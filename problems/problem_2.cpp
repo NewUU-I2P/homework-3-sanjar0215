51b44445cefcd16ed59e88df86059785c251323c
sanjar0215 <131523769+sanjar0215@users.noreply.github.com>
2023-11-14 17:42:21 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,7 +1,6 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
-    cin >> i >> j >> k;
     result = (i > j) ? ( i > k ? i : k ) : ( j > k ? j : k );
     return result;
 }
